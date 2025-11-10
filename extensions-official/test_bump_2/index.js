96e6f498dbe2647ebda6c304f7c1d27817696d3c
Johnson Fung <johnson.ciyu.fung@gmail.com>
2025-11-09 16:14:01 -0800

    Test: Add test extension

---
+++ extensions-official/test_bump_2/index.js
@@ -0,0 +1,16 @@
+(function() {
+  var extensionRoot = new Extension("test_bump_2", "1.0.0");
+
+  var test_command = new Command(
+    "test_command",
+    [],
+    "insert",
+    "Test command",
+    [],
+    extensionRoot
+  );
+
+  test_command.execute = function(payload) {
+    return new ReturnObject("success", "Test successful", "Test content");
+  };
+})();
